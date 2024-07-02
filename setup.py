6880cae21dbd20d59cfa6649183ef27884b3db89
Lucy-Shichman <lgs4zj@virginia.edu>
2024-07-02 11:03:03 -0400

    mypackage files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='1.0.0',
+    url='https://github.com/Lucy-Shichman/module_9_exercise',
+    author='Lucy Shichman',
+    author_email='lgs4zj@virginia.edu',
+    description='This is my package',
+    packages=['mypackage']
+)
